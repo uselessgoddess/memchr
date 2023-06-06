57ebb9e4a1e9a3445a124d82555509edca6e1cc4
uselessgoddess <dodickgod@gmail.com>
2023-06-06 16:03:53 +0300

    Add `AVX2` implementation by GPT

--- src/lib.rs
+++ src/lib.rs
@@ -5,41 +5,89 @@ pub mod naive {
 }
 
 pub mod gpt {
-    use std::arch::x86_64::*;
-    use std::mem;
+    use std::arch::x86_64 as arch;
 
-    const SIMD_LANE_SIZE: usize = mem::size_of::<__m128i>();
+    #[target_feature(enable = "sse2")]
+    unsafe fn find_char_in_blocks(
+        buf: &[u8],
+        ch: u8,
+        block_size: usize,
+        repeat_size: usize,
+    ) -> Result<usize, usize> {
+        let ch_repeat = arch::_mm_set1_epi8(ch as i8);
+
+        let mut offset = 0;
+        while offset + repeat_size - 1 < buf.len() {
+            let read_ptr = buf.as_ptr().add(offset) as *const arch::__m128i;
+            let block_ch = arch::_mm_loadu_si128(read_ptr);
+            let cmp_res = arch::_mm_cmpeq_epi8(block_ch, ch_repeat);
+
+            let cmp_mask = arch::_mm_movemask_epi8(cmp_res);
+            if cmp_mask != 0 {
+                return Ok(offset + cmp_mask.trailing_zeros() as usize);
+            }
+
+            offset += block_size;
+        }
+
+        Err(offset)
+    }
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn find_char_in_avx2_blocks(
+        buf: &[u8],
+        ch: u8,
+        block_size: usize,
+        repeat_size: usize,
+    ) -> Result<usize, usize> {
+        let ch_repeat = arch::_mm256_set1_epi8(ch as i8);
+
+        let mut offset = 0;
+        while offset + repeat_size - 1 < buf.len() {
+            let read_ptr = buf.as_ptr().add(offset) as *const arch::__m256i;
+            let block_ch = arch::_mm256_loadu_si256(read_ptr);
+            let cmp_res = arch::_mm256_cmpeq_epi8(block_ch, ch_repeat);
+
+            let cmp_mask = arch::_mm256_movemask_epi8(cmp_res);
+            if cmp_mask != 0 {
+                return Ok(offset + cmp_mask.trailing_zeros() as usize);
+            }
+
+            offset += block_size;
+        }
+
+        Err(offset)
+    }
 
     pub fn memchr(needle: u8, haystack: &[u8]) -> Option<usize> {
-        let needle_simd = unsafe { _mm_set1_epi8(needle as i8) };
-        let len = haystack.len();
-        let mut i = 0;
+        const SSE2_BLOCK_SIZE: usize = 16;
+        const AVX2_BLOCK_SIZE: usize = 32;
 
-        let haystack_ptr = haystack.as_ptr();
+        const THRESHOLD: usize = 32;
 
-        while (len - i) >= SIMD_LANE_SIZE {
-            unsafe {
-                let chunk = _mm_loadu_si128(haystack_ptr.add(i) as *const _);
-                let mask = _mm_cmpeq_epi8(chunk, needle_simd);
-                let mask_bits = _mm_movemask_epi8(mask);
-
-                if mask_bits != 0 {
-                    let first_nz = mask_bits.trailing_zeros();
-                    return Some(i + first_nz as usize);
-                }
-            }
-            i += SIMD_LANE_SIZE;
+        if haystack.len() < THRESHOLD {
+            // Для коротких буферов используем обычный поиск.
+            return haystack.iter().position(|&b| b == needle);
         }
 
-        // Check the remaining part of the buffer
-        for &b in haystack[i..].iter() {
-            if b == needle {
-                return Some(i);
-            }
-            i += 1;
-        }
+        let ch = needle;
+        let buf = haystack;
 
-        None
+        let result = if is_x86_feature_detected!("avx2") {
+            unsafe { find_char_in_avx2_blocks(buf, ch, AVX2_BLOCK_SIZE, 2 * AVX2_BLOCK_SIZE) }
+        } else if is_x86_feature_detected!("sse2") {
+            unsafe { find_char_in_blocks(buf, ch, SSE2_BLOCK_SIZE, 2 * SSE2_BLOCK_SIZE) }
+        } else {
+            Err(0)
+        };
+
+        match result {
+            Ok(pos) => Some(pos),
+            Err(offset) => {
+                let remaining_buf = &buf[offset..];
+                remaining_buf.iter().position(|&c| c == ch).map(|pos| offset + pos)
+            }
+        }
     }
 }
 
